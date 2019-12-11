287a2de9057dbcbeada8851866f2b26d1394794d
liorfishman <lior.fishman96@gmail.com>
2019-12-11 16:37:26 +0200

    unsorted patch

--- format_files/format_CpG_seq.py
+++ format_files/format_CpG_seq.py
@@ -9,6 +9,7 @@ import format_files.tools as tools
 
 PATIENT_FILE_FORMAT = "CRC*.pickle.zlib"
 FULL_CPG_FILE_FORMAT = "full_cpg_seq_chr*.pickle.zlib"
+OUTPUT_FILE_FORMAT = "full_cpg_ratios_patient%chr%s.pickle.zlib"
 PATIENT_FILE_DETAILS_RE = re.compile(".+(CRC\d+)_.+_(chr\d+).pickle.zlib")
 FULL_CPG_FILE_DETAILS_RE = re.compile(".+full_cpg_seq_(chr\d+).pickle.zlib")
 
@@ -32,7 +33,7 @@ def create_chr_df(chr_file_list, chr_cpg_pos):
     i = 1
     for chr_path in chr_file_list:
         cell = tools.load_compressed_pickle(chr_path)
-
+        cell = cell[cell[:,0].argsort()]
         # cell_indexes = np.where(
         #     np.logical_and(cell[:, POSITION_INDEX] > start, cell[:, POSITION_INDEX] < end))
         # pmd = cell[cell_indexes]
@@ -51,6 +52,7 @@ def main():
     if not output:
         output = os.path.dirname(sys.argv[0])
 
+    # get dict chr#: array[cpg]
     full_cpg_files_path = os.path.join(args.folder, FULL_CPG_FILE_FORMAT)
     all_full_cpg_file_paths = glob.glob(full_cpg_files_path)
     chr_pos_dict = {}
@@ -62,12 +64,14 @@ def main():
         full_cpg_seq = tools.load_compressed_pickle(file_path)
         chr_pos_dict[chr].append(full_cpg_seq)
 
+    # patient files path
     patient_files_path = os.path.join(args.folder, PATIENT_FILE_FORMAT)
     if args.patient:
         patient_files_path = os.path.join(args.folder, "%s" % args.patient + PATIENT_FILE_FORMAT)
-
     all_patient_file_paths = glob.glob(patient_files_path)
     patient_chr_dict = {}
+
+    # get dict patient#: array[cell]
     for file_path in all_patient_file_paths:
         name = PATIENT_FILE_DETAILS_RE.findall(file_path)[0]
         if name not in patient_chr_dict:
@@ -77,6 +81,7 @@ def main():
 
     for patient_chr in patient_chr_dict:
         chr_all_cells = create_chr_df(patient_chr_dict[patient_chr], chr_pos_dict[patient_chr[1]][0])
+        tools.save_as_compressed_pickle()
 
 
 if __name__ == '__main__':
