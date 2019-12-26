8b5fd1f0b4b3ba31d64ca7690cb77c87a7b47ad8
Dror Bar <drorspace94@gmail.com>
2019-12-26 11:07:39 +0200

    add fix files to the git

--- stats/across_sample_stats.py
+++ stats/across_sample_stats.py
@@ -5,6 +5,10 @@ import os
 import re
 import sys
 
+import matplotlib
+
+matplotlib.use("Agg")
+
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
@@ -13,21 +17,22 @@ sys.path.append(os.path.dirname(os.getcwd()))
 import numpy as np
 import pandas as pd
 
-FOLDER_SUFFIX = os.path.join("CRC09", "all_cpg_ratios_CRC*_chr*.dummy.pkl.zip")
+FOLDER_SUFFIX = os.path.join("*", "all_cpg_ratios_CRC*_chr*.dummy.pkl.zip")
+
 CHR_RE = re.compile(".*chr(\d+).*")
 PATIENT_RE = re.compile("CRC\d\d")
 COLUMNS = ["read_number", "counter"]
 
 CENETROMERE_DICT = {
-    "01": [121311574, 129523877],
-    "02": [91278726, 97911435],
-    "03": [88724336, 93610603],
-    "04": [48396285, 52374061],
-    "05": [45111338, 50515299],
-    "06": [57556887, 63334797],
-    "07": [57041911, 63035444],
-    "08": [43719124, 48091035],
-    "09": [47315670, 51717126],
+    "1": [121311574, 129523877],
+    "2": [91278726, 97911435],
+    "3": [88724336, 93610603],
+    "4": [48396285, 52374061],
+    "5": [45111338, 50515299],
+    "6": [57556887, 63334797],
+    "7": [57041911, 63035444],
+    "8": [43719124, 48091035],
+    "9": [47315670, 51717126],
     "10": [38196156, 42596634],
     "11": [52073942, 56281937],
     "12": [33028390, 38938733],
@@ -115,7 +120,7 @@ def create_chromosome_img(positions, reads, chr_number, output_folder):
     ax_scatter.scatter(med_pos, med_reads, c=med_reads)
     ax_scatter.set_ylim(top=max(med_reads))
 
-    all_pos = med_pos[::400]
+    all_pos = med_pos[::1000]
     all_pos += CENETROMERE_DICT[chr_number]
 
     ax_scatter.set_xticks(all_pos)
@@ -125,6 +130,20 @@ def create_chromosome_img(positions, reads, chr_number, output_folder):
     ax_scatter.set_xticklabels(all_labels)
 
     plt.savefig(os.path.join(output_folder, "coverage_along_chr_%s.png" % chr_number))
+
+
+def fix_counter(file_name):
+    new_file = file_name.replace(".csv", "_new.csv")
+    data = open(file_name, "r").readlines()
+
+    c = collections.Counter()
+    for line in data[1:]:
+        _, number, counter = line.strip().split("w")
+        c.update({round(float(number) * 100): int(counter)})
+
+    counter_df = pd.DataFrame.from_dict(c, orient='index').reset_index()
+    counter_df.columns = COLUMNS
+    counter_df.to_csv(new_file, "w")
 
 
 def main():
