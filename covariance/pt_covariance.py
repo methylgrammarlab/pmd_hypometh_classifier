804c256b966e45eaf460b547e8417472dc4d036c
liorf <lior.fishman@mail.huji.ac.il>
2020-02-13 14:59:41 +0200

    Average covariance bedgraphs - documentation

--- covariance/pt_covariance.py
+++ covariance/pt_covariance.py
@@ -11,13 +11,13 @@ import sys
 
 BEDGRAPH_LINE_FORMAT = "{chr_name}\t{start}\t{end}\t{number}\n"
 BEDGRPH_OUTPUT_FILE = "covariance_between_CpG_%s.bedgraph"
-BEDGRAPH_OUTPUT_FILE_FORMAT = "average_covariance_between_cpg_%s_chr_%s_group_%s.bedgraph"
+BEDGRAPH_OUTPUT_FILE_FORMAT = "average_covariance_between_cpg_%s_chr_%s_region_%s.bedgraph"
 WINDOWS_SIZE = 5000
 
 CPG_FORMAT_FILE_FORMAT = "all_cpg_ratios_*_%s.dummy.pkl.zip"
 CPG_FORMAT_FILE_RE = re.compile(".+(CRC\d+)_chr(\d+).dummy.pkl.zip")
 
-GROUPS = ['PT', 'NC']
+REGIONS = ['PT']
 
 
 def parse_input():
@@ -29,21 +29,30 @@ def parse_input():
     return args
 
 
-def create_group_bedgraph(file, patient, chromosome, group, output):
+def create_region_bedgraph(file, patient, chromosome, region, output):
+    """
+    Creates a bedgraph of the mean covariance of cPg's, using data only from one region at a time, adding the NC. The
+    covariance is calculated in windows.
+    :param file: The filename of the file with the parsed scWGBS data
+    :param patient: Patient ID of the current data
+    :param chromosome: The current chromosome
+    :param region: The region ID
+    :param output: The output folder
+    """
     df = pd.read_pickle(file)
-    pt_index = [pt for pt in df.index if pt.startswith(group)]
-    pt_df = df.loc[pt_index, :]
+    region_cell_ids = [cell_id for cell_id in df.index if cell_id.startswith(region)] + [cell_id for cell_id in df.index if cell_id.startswith('NC')]
+    region_df = df.loc[region_cell_ids, :]
     # num_of_cpg = 100
-    num_of_cpg = pt_df.shape[1]
-    output_filename = os.path.join(output, BEDGRAPH_OUTPUT_FILE_FORMAT % (patient, chromosome, group))
+    num_of_cpg = region_df.shape[1]
+    output_filename = os.path.join(output, BEDGRAPH_OUTPUT_FILE_FORMAT % (patient, chromosome, 'PTandNC'))
 
     # TODO covariance with min_periods=10
     with open(output_filename, "w") as output_file:
         for i in tqdm(range(0, num_of_cpg, WINDOWS_SIZE)):
-            pmd_columns = pt_df.columns[i:min(i + WINDOWS_SIZE, num_of_cpg)]
-            covariance = pt_df.loc[:, pmd_columns].cov()
+            cur_columns_inds = region_df.columns[i:min(i + WINDOWS_SIZE, num_of_cpg)]
+            covariance = region_df.loc[:, cur_columns_inds].cov()
             average_covariance = covariance.mean()
-            for cpg in pmd_columns:
+            for cpg in cur_columns_inds:
                 start = cpg
                 end = cpg + 1
                 number = average_covariance[cpg]
@@ -54,8 +63,15 @@ def create_group_bedgraph(file, patient, chromosome, group, output):
 
 
 def create_bedgraphs(file, patient, chromosome, output):
-    for group in GROUPS:
-        create_group_bedgraph(file, patient, chromosome, group, output)
+    """
+    Goes over all the regions and calls the function to create a bedgraph for each one.
+    :param file: The filename of the file with the parsed scWGBS data
+    :param patient: Patient ID of the current data
+    :param chromosome: The current chromosome
+    :param output: The output folder
+    """
+    for region in REGIONS:
+        create_region_bedgraph(file, patient, chromosome, region, output)
 
 
 def main():
