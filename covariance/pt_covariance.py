4530f66bb6b5371d11ce9aba42f83f86ca4141f5
liorf <lior.fishman@mail.huji.ac.il>
2020-02-13 14:18:17 +0200

    Average covariance bedgraphs - bug fix

--- covariance/pt_covariance.py
+++ covariance/pt_covariance.py
@@ -17,7 +17,7 @@ WINDOWS_SIZE = 5000
 CPG_FORMAT_FILE_FORMAT = "all_cpg_ratios_*_%s.dummy.pkl.zip"
 CPG_FORMAT_FILE_RE = re.compile(".+(CRC\d+)_chr(\d+).dummy.pkl.zip")
 
-GROUPS = ['PT']
+GROUPS = ['PT', 'NC']
 
 
 def parse_input():
@@ -30,17 +30,18 @@ def parse_input():
 
 
 def create_group_bedgraph(file, patient, chromosome, group, output):
-    to_read = file
-    df = pd.read_pickle(to_read)
+    df = pd.read_pickle(file)
     pt_index = [pt for pt in df.index if pt.startswith(group)]
     pt_df = df.loc[pt_index, :]
-    # num_of_cpg = 10000
+    # num_of_cpg = 100
     num_of_cpg = pt_df.shape[1]
+    output_filename = os.path.join(output, BEDGRAPH_OUTPUT_FILE_FORMAT % (patient, chromosome, group))
 
-    with open(os.path.join(output, BEDGRAPH_OUTPUT_FILE_FORMAT % (patient, chromosome, group)), "w") as output_file:
+    # TODO covariance with min_periods=10
+    with open(output_filename, "w") as output_file:
         for i in tqdm(range(0, num_of_cpg, WINDOWS_SIZE)):
             pmd_columns = pt_df.columns[i:min(i + WINDOWS_SIZE, num_of_cpg)]
-            covariance = df.loc[:, pmd_columns].cov()
+            covariance = pt_df.loc[:, pmd_columns].cov()
             average_covariance = covariance.mean()
             for cpg in pmd_columns:
                 start = cpg
