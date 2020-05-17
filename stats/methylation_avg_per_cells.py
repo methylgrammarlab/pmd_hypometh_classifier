8fa19155c8e9108fb28e5202965b30fd836d57ec
liorf <lior.fishman@mail.huji.ac.il>
2020-05-17 23:54:47 +0300

    added more graph options, color by region

--- stats/methylation_avg_per_cells.py
+++ stats/methylation_avg_per_cells.py
@@ -37,15 +37,17 @@ NAN_COLOUR = '#9a9a9a'
 
 # colors used in paper
 SUBLINEAGE_COLOURS = {'A0': '#F8B4C0', 'A1': '#E0E346', 'A2': '#6F51A1', 'A3': '#F89C31', 'A4': '#EF292A',
-                   'A5': '#A45AA4',
-                   'A6': '#993232', 'A7': '#2256A6', 'A8': '#BC84BA', 'A9': '#ED3095', 'B': '#3B86C6',
-                   'B0': '#3B86C6',
-                   'B1': '#66CAD4', 'B2': '#6D8841', 'B3': '#28898A', 'C0': '#E7CFA0', 'C1': '#DDBD7E',
-                   'C2': '#D1A34A',
-                   'C3': '#B89979', 'C4': '#AA845D', 'C5': '#8C6E4A', 'undefined': '#BBBABC'}
+                      'A5': '#A45AA4',
+                      'A6': '#993232', 'A7': '#2256A6', 'A8': '#BC84BA', 'A9': '#ED3095', 'B': '#3B86C6',
+                      'B0': '#3B86C6',
+                      'B1': '#66CAD4', 'B2': '#6D8841', 'B3': '#28898A', 'C0': '#E7CFA0', 'C1': '#DDBD7E',
+                      'C2': '#D1A34A',
+                      'C3': '#B89979', 'C4': '#AA845D', 'C5': '#8C6E4A', 'undefined': '#BBBABC'}
 REGION_COLOURS = {'NC': '#6FBE44', 'PT1': '#F57E32', 'PT2': '#3C8A45', 'PT3': '#3C8A45', 'PT4': '#53ABDA',
-                  'PT5': '#EBE94F', 'PT6': '#EFB0BC', 'LN1': '#B6B6BA', 'LN2': '#B28A8E', 'LN3': '#B28A8E','LN4': '#67532B', 'LN5': '#514321',
-                  'ML1': '#94D6E4', 'ML2': '#4872B7', 'ML3': '#1C49A0', 'ML4': '#333463', 'ML5': '#464B7D', 'ML6': '#3C3E69', 'MP1': '#CFB0D3',
+                  'PT5': '#EBE94F', 'PT6': '#EFB0BC', 'LN1': '#B6B6BA', 'LN2': '#B28A8E', 'LN3': '#B28A8E',
+                  'LN4': '#67532B', 'LN5': '#514321',
+                  'ML1': '#94D6E4', 'ML2': '#4872B7', 'ML3': '#1C49A0', 'ML4': '#333463', 'ML5': '#464B7D',
+                  'ML6': '#3C3E69', 'MP1': '#CFB0D3',
                   'MP2': '#BC85BB', 'MP3': '#8254A2', 'MP4': '#842F8D', 'MP5': '#632E65', 'MO1': '#E7CF9F',
                   'MO2': '#E2C68E', 'MO3': '#E2C68E', 'MO4': '#D9B36B', 'MO5': '#D5AB5B', 'MO6': '#D1A349'}
 
@@ -103,6 +105,10 @@ def get_sublineage(cell, sublineage_info, patient):
         return "undefined"
 
 
+def get_region(cell):
+    return cell.split('_')[0]
+
+
 def get_colours(key, col_name):
     if col_name == 'sublineage':
         return SUBLINEAGE_COLOURS[key]
@@ -118,33 +124,34 @@ def get_legend_title(col_name):
 
 
 def create_colored_graphs(df, col_name, patient):
-    values = df.loc[df.patient == patient, 'median'].sort_values()
+    df = df.sort_values(by=['median'])
+    values = df.loc[df.patient == patient, 'median']
     colors = [get_colours(key, col_name) for key in df.loc[df.patient == patient, col_name]]
     x = [i for i in range(values.shape[0])]
-    legend_names = df.loc[df.patient == patient, col_name].unique()
+    legend_names = sorted(df.loc[df.patient == patient, col_name].unique())
     custom_lines = [Line2D([0], [0], color=get_colours(key, col_name), ls=' ', marker='s', ms=12) for key in
                     legend_names]
-    plt.bar(x, values, color=colors, width =1)
+    plt.bar(x, values, color=colors, width=1)
     plt.xlabel("tumor cells (self sorted)")
     plt.ylabel("avg meth")
     title = get_legend_title(col_name)
     lgd = plt.legend(custom_lines, legend_names, loc='center left', bbox_to_anchor=(1.0, 0.5),
                      title=title)
     plt.title("cell methylation means for %s" % patient)
-    plt.savefig("cell_methylation_means_for_%s_colored_%s.png" % (patient, col_name), bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.savefig("cell_methylation_means_for_%s_colored_%s.png" % (patient, col_name), bbox_extra_artists=(lgd,),
+                bbox_inches='tight')
     plt.close()
 
 
-def create_graphs():
-    df = pd.read_pickle("all_patients_avg_data.pickle.zlib")
+def create_graphs(path):
+    df = pd.read_pickle(path)
+    # for patient in ['CRC01', 'CRC10', 'CRC11', 'CRC13']:
     for patient in df.patient.unique():
         create_colored_graphs(df, 'sublineage', patient)
         create_colored_graphs(df, 'region', patient)
 
 
 def main():
-    create_graphs()
-    return
     args = parse_input()
 
     methylation_folder = args.methylation_folder
@@ -176,25 +183,14 @@ def main():
         patient_df.loc[:, 'sublineage'] = pa_df_meth.index
         patient_df.loc[:, 'sublineage'] = patient_df.loc[:, 'sublineage'].apply(get_sublineage,
                                                                                 args=(sublineage, patient))
+        patient_df.loc[:, 'region'] = pa_df_meth.index
+        patient_df.loc[:, 'region'] = patient_df.loc[:, 'region'].apply(get_region)
 
         all_patients.append(patient_df)
+
     all_patients_df = pd.concat(all_patients)
     all_patients_df.to_pickle("all_patients_avg_data.pickle.zlib")
-
-    # values = pa_df_meth.median(axis=1).sort_values()
-    # colors = [get_colours(sublineage, patient, cell) for cell in values.index]
-    # x = [i for i in range(values.shape[0])]
-    # legend_names = sorted({get_sublineage(sublineage, patient, cell) for cell in values.index})
-    # custom_lines = [Line2D([0], [0], color=CONVERT_COLOURS[i], ls=' ', marker='s', ms=12) for i in
-    #                 legend_names]
-    # plt.bar(x, values, color=colors)
-    # plt.xlabel("tumor cells (self sorted)")
-    # plt.ylabel("avg meth")
-    # lgd = plt.legend(custom_lines, legend_names, loc='center left', bbox_to_anchor=(1.0, 0.5),
-    #                  title='Genetic\nsub-lineages:')
-    # plt.title("cell methylation means for %s" % patient)
-    # plt.savefig("cell_methylation_means_for_%s_paperc.png" % patient, bbox_extra_artists=(lgd,), bbox_inches='tight')
-    # plt.close()
+    create_graphs("all_patients_avg_data.pickle.zlib")
 
 
 if __name__ == '__main__':
